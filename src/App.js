74194edc549f53853ec3c935dac08a6f8afd6d37
YjjTT <yjjtt@tempot.com>
2019-05-13 13:30:41 +0800

    redux devtools

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux' 
 import './App.css';
-
+import * as userActions from '../src/actions/user'
 class App extends Component {
   render() {
     console.log(this.props);
+
+    const { user } = this.props;
+
     return (
       <div className="App">
       <header className="App-header">
         redux-test
+        <div>
+          {user.isLogin?'欢迎你':'未登录'}
+        </div>
+        <button onClick={()=> this.props.dispatch(userActions.login({account: 'test', password: '123'}))}>登录</button>
       </header>
     </div>
     )
@@ -16,6 +23,6 @@ class App extends Component {
 }
 
 const mapState = state => ({
-  state,
+  user: state.user,
 })
 export default connect(mapState) (App);
--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,5 @@ import {createStore} from 'redux'
 import reducer from '../reducer'
 
 export default function Store(initState){
-  return createStore(reducer, initState);
+  return createStore(reducer, initState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 }
